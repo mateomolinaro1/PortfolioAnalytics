186d83735f7dae9368daffafc2bafc4e64f79233
mateomolinaro1 <118679896+mateomolinaro1@users.noreply.github.com>
2024-11-11 14:10:15 +0100

    Update analysis.py

--- code/my_package/analysis.py
+++ code/my_package/analysis.py
@@ -5,14 +5,77 @@ Created on Tue Nov  5 20:33:47 2024
 @author: mateo
 """
 import math
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import statsmodels.api as sm
 from typing import Union, Optional, Tuple
 
 from my_package.exceptions import FinancialAssetError
 
+class DataChecks:
+    """
+    A class containing methods for performing validation checks on dataframes used in financial analysis.
 
+    The purpose of this class is to ensure that the 'x' and 'y' dataframes passed to it are correctly formatted
+    for further analysis. The checks include validating column structure, data types, row counts, and index consistency.
+    """
+    
+    @staticmethod
+    def check_dataframe(x: pd.DataFrame, y: pd.DataFrame) -> None:
+        """
+        Validates the structure and content of the 'x' and 'y' DataFrames.
+
+        This method checks that the 'y' DataFrame has 2 columns, with the first column named 'date' and 
+        containing datetime values, and the second column containing float values. It also ensures that the
+        index of 'y' starts at 0. Additionally, it validates the 'x' DataFrame to ensure it has at least one column,
+        two rows, and that all columns contain numeric values. The method also checks that the index of 'x' starts
+        at 0 and that both DataFrames have the same number of rows.
+
+        Parameters:
+        x (pd.DataFrame): The 'x' DataFrame to validate, containing numerical data.
+        y (pd.DataFrame): The 'y' DataFrame to validate, with 'date' as the first column and numerical values in the second column.
+
+        Raises:
+        AssertionError: If 'y' does not meet the specified structural or data type requirements.
+        ValueError: If 'x' does not meet the specified structural or data type requirements or if the number of rows in 'x' and 'y' are mismatched.
+        """
+        
+        # Data validation checks for 'y' DataFrame
+        if y.shape[1] != 2:
+            raise AssertionError("The 'y' DataFrame must have 2 columns.")
+        # Check if y has at least two rows
+        if y.shape[0] < 2:
+            raise ValueError("DataFrame 'x' must have at least two rows.")
+        if y.columns[0] != 'date':
+            raise AssertionError("The name of the first column must be 'date'.")
+        if not pd.api.types.is_datetime64_any_dtype(y.iloc[:, 0]):
+            raise AssertionError("The first column must be of datetime type.")
+        if not pd.api.types.is_float_dtype(y.iloc[:, 1]):
+            raise AssertionError("The second column must be of float type.")
+        if y.index[0] != 0:
+            raise AssertionError("The index of 'y' must start at 0.")
+        
+        # Data validation checks for 'x' DataFrame
+        # Check if x has at least one column
+        if x.shape[1] < 1:
+            raise ValueError("DataFrame 'x' must have at least one column.")
+        # Check if x has at least two rows
+        if x.shape[0] < 2:
+            raise ValueError("DataFrame 'x' must have at least two rows.")
+        # Check if all columns in x are numeric
+        if not all(pd.api.types.is_numeric_dtype(x[col]) for col in x.columns):
+            raise ValueError("All columns in 'x' must be numeric.")
+        # Check if the index starts at 0
+        if x.index[0] != 0:
+            raise ValueError("Index of DataFrame 'x' must start at 0.")
+        # Check if x and y have the same number of rows
+        if x.shape[0] != y.shape[0]:
+            raise ValueError("DataFrame 'x' and 'y' must have the same number of rows.")
+            
+        return
+    
 class FinancialAssetUtilities:
     """
     A utility class for performing financial calculations on price data.
@@ -176,17 +239,121 @@ class DescriptiveStats:
         return df
 
 
-class FactorAnalysis:
+class FactorAnalysis(DataChecks):
     """
     A class to create customizable functions for factor analysis.
     """
     
     @staticmethod
-    def calculate_factor_decomposition(df: pd.DataFrame) -> pd.DataFrame:
+    def rolling_regression(y: pd.DataFrame, x:pd.DataFrame, window:int, add_const:bool):
+        """
+        Perform a rolling window linear regression on the provided 'x' and 'y' dataframes.
+    
+        This method applies a rolling regression with a specified window size and optionally adds a constant to 
+        the 'x' data for each regression. The function checks the structure of the input dataframes and validates 
+        the parameters. It performs an Ordinary Least Squares (OLS) regression over each window and stores the 
+        regression coefficients in a new dataframe, returning the results after removing any NaN values.
+    
+        Parameters:
+        y (pd.DataFrame): A dataframe containing the dependent variable. The first column should be 'date' (datetime), 
+                          and the second column should be numeric (float).
+        x (pd.DataFrame): A dataframe containing the independent variables. All columns must be numeric.
+        window (int): The size of the rolling window for the regression.
+        add_const (bool): Whether or not to add a constant (intercept) to the regression model.
+    
+        Returns:
+        pd.DataFrame: A dataframe containing the regression coefficients (including 'date' and possibly a constant) 
+                      for each window, with NaN values dropped.
+    
+        Raises:
+        ValueError: If 'window' is not an integer or 'add_const' is not a boolean.
+        AssertionError: If the 'x' or 'y' dataframes do not meet the required structure (e.g., correct column names, 
+                        data types, and index starting at 0).
+        """
+        # x sans constante, col1 = dates, col2 = facteur 1. y avec date
         
+        # Dataframes check
+        DataChecks.check_dataframe(x, y)
         
+        # Check if window is an integer
+        if not isinstance(window, int):
+            raise ValueError("The variable 'window' must be an integer.")
         
-        return
+        # Check if add_const is a boolean
+        if not isinstance(add_const, bool):
+            raise ValueError("The variable 'add_const' must be a boolean.")
+            
+        # Storing
+        names = x.columns.tolist()  # Column name extraction
+        df_rolling_regression = pd.DataFrame(np.nan, index=range(x.shape[0]), columns=names)  # DataFrame creation
+        df_rolling_regression.insert(0, 'date', pd.NaT)  # Insert 'date' column at the start
+        
+        # Constant for regression
+        if add_const == True:
+            x = sm.add_constant(x)
+            df_rolling_regression.insert(loc=1, column='constant', value=[np.nan] * len(df_rolling_regression))
+
+        for i in range(window, x.shape[0]):
+            
+            x_window = x.iloc[i - window: i, :]
+            y_window = y.iloc[i - window: i, 1].to_frame()
+            model = sm.OLS(y_window, x_window, missing='drop').fit()
+            date = y.iloc[i, 0]
+            b = pd.DataFrame(model.params).T
+            b.insert(loc=0, column='date', value=date)
+            b['date'] = pd.to_datetime(b['date'])
+            df_rolling_regression.iloc[i] = b.iloc[0, :]
+        
+        return df_rolling_regression.dropna()
+    
+    @staticmethod
+    def calculate_factor_decomposition(y: pd.DataFrame, x: pd.DataFrame, window:int, add_const:bool) -> pd.DataFrame:
+        # y avec dates, x sans constante
+        
+        # Dataframes check
+        DataChecks.check_dataframe(x, y)
+        
+        # Check if window is an integer
+        if not isinstance(window, int):
+            raise ValueError("The variable 'window' must be an integer.")
+        
+        # Check if add_const is a boolean
+        if not isinstance(add_const, bool):
+            raise ValueError("The variable 'add_const' must be a boolean.")
+            
+        df_rolling_regression = FactorAnalysis.rolling_regression(y, x, window, add_const)
+        df_betas = df_rolling_regression.iloc[:,1:]
+        X = x.iloc[window:,:]
+        Y = y.iloc[window:,1:]
+        Y_np = Y.values
+        Y_np = Y_np.flatten() 
+        dates = y.iloc[window:,0]
+        dates = dates.reset_index(drop=True)
+        
+        # Using numpy for vectorization
+        betas = df_betas.values
+        X_np = X.values
+        betas_times_factors = X_np*betas
+        y_pred = np.sum(betas_times_factors, axis=1)
+        resid = y_pred - Y_np
+        betas_times_factors = np.column_stack((betas_times_factors, -1*resid))
+        
+        Y_np_reshaped = np.repeat(Y_np[:, np.newaxis], betas_times_factors.shape[1], axis=1)
+        # factor_repartition_percentage = np.where(Y_np_reshaped != 0, betas_times_factors / Y_np_reshaped * 100, np.nan)
+        
+        factor_repartition_percentage = np.where((Y_np_reshaped != 0) & np.isfinite(Y_np_reshaped), 
+                                         betas_times_factors / Y_np_reshaped * 100, 
+                                         np.nan)
+        
+        # check = np.sum(factor_repartition_percentage, axis=1)
+        
+        # check_y = y_pred - resid
+        
+        names = x.columns.to_list()
+        names.append('residual')
+        df_factor_decomposition = pd.DataFrame(factor_repartition_percentage, columns=names)
+        df_factor_decomposition.insert(0, 'date', dates)
+        return df_factor_decomposition
 
 class CreateVisuals:
     """
@@ -285,5 +452,4 @@ class CreateVisuals:
     
         # Show the plot
         plt.show()
-
 
