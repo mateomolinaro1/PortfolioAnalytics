e0c7460e847fdb113ef51dafced7a678d6b2f59e
mateomolinaro1 <118679896+mateomolinaro1@users.noreply.github.com>
2024-11-26 21:00:15 +0100

    Update exceptions.py

--- code/my_package/exceptions.py
+++ code/my_package/exceptions.py
@@ -36,8 +36,33 @@ class MissingPriceColumnError(FinancialAssetError):
     def __init__(self):
         super().__init__("DataFrame must contain a 'price' column.")
 
+class IncorretInputType(FinancialAssetError):
+    """
+    Exception raised when the type or dtype of an input is incorrect.
+    """
+    def __init__(self, input_type=None, input_dtype=None, required_type=None, required_dtype=None, d_type=False):
+        self.input_type = input_type
+        self.input_dtype = input_dtype
+        self.required_type=required_type
+        self.required_dtype = required_dtype
+        self.d_type = d_type
+        if not d_type:
+            super().__init__(f"The type of the input: '{input_type}' is incorrect. Must be of type: '{required_type}'")
+        else:
+            super().__init__(f"The dtype of the input: '{input_dtype}' is incorrect. Must be of dtype: '{required_dtype}'")
+            
+class NotEnoughData(FinancialAssetError):
+    """
+    Exception raised when there is not enough data point provided.
+    """
+    def __init__(self, required_nb_data_point=None):
+        self.required_nb_data_point = required_nb_data_point
+        super().__init__(f"Not enough data point(s) provided. Minimum number of data point(s) required: {required_nb_data_point}")
+        
 
 # Assignation des exceptions comme attributs de FinancialAssetError
 FinancialAssetError.InvalidPriceError = InvalidPriceError
 FinancialAssetError.InvalidMethodError = InvalidMethodError
 FinancialAssetError.MissingPriceColumnError = MissingPriceColumnError
+FinancialAssetError.IncorrectInputType = IncorretInputType
+FinancialAssetError.NotEnoughData = NotEnoughData
